5a420fdb8a97ecba39f8cf439c39adb80a6926bd
BaiTapBongDa-web <quang.vlog2018@gmail.com>
2026-09-17 23:06:22 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -65,7 +65,7 @@ Phong cách:
 def home():
     return send_from_directory(
         os.path.dirname(os.path.abspath(__file__)),
-        "TrangWebLenYTuongBaiTapBongDaCT (2).html"
+        ""index.html""
     )
 
 
